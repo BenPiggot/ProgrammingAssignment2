2c6c5a645c44523bf4d458460e849e36237143ca
wbp <wbpiggot@gmail.com>
2014-04-23 20:02:26 -0700

    saving cachematrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of these two functions together is to create a way to store 
+## or cache the inverse of a matrix produced by the solve() function. 
 
-## Write a short comment describing this function
+
+## The function makeCacheMatrix (below) takes a matrix as an argument and 
+## returns a list with four functions. This list of four functions serve 
+## to cache the matrix the makeCacheMatrix function takes as an argument 
+## in an object in an environment outside that defined by the function.
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -18,7 +22,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The function cacheSolve (below) searches to see if the inverse of the
+##  matrix has already been calculated and stored somewhere in the R 
+## environment. If so, it returns the inverse stored in the object 
+## it finds. If it cannot find the inverse, the function will calculate 
+## the inverse itself.
 
 cacheSolve <- function(x, ...) {
     m <- x$getsolve()
@@ -31,6 +39,5 @@ cacheSolve <- function(x, ...) {
     x$setsolve(m)
     m
   } 
-
 
 
